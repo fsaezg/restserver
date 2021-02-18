cc9050bbaa76a63afbd0a2945b7654cbc0169005
Francisco <fasaezguerra@gmail.com>
2021-02-18 14:45:28 -0300

    Primer commit

---
+++ models/server.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const cors = require('cors');
+
+class Server {
+
+    constructor() {
+        this.app = express();
+        this.port = process.env.PORT;
+        this.usuariosPath = '/api/usuarios';
+
+        // Middlewares = Función para añadir funcionalidades
+        this.middlewares();
+
+        // Rutas de mi aplicación
+
+        this.routes();
+    }
+
+    middlewares() {
+        
+        // CORS
+        this.app.use( cors() );
+
+        // Lectura y Parseo del Body
+        this.app.use( express.json() );
+
+        // Directorio Público
+        this.app.use( express.static( 'public' ) );
+
+    }
+
+    routes() {
+        
+        this.app.use( this.usuariosPath, require('../routes/usuarios'));
+
+    }
+
+    listen() {
+        this.app.listen( this.port, () => {
+            console.log('Servidor corriendo en el puerto', this.port );
+        });
+    }
+
+}
+
+module.exports = Server;
